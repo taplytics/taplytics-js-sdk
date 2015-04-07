6fd8b564c8c12ca530619de5c9ae0d04be60e3ff
Nima Gardideh <nima@halfmoon.ws>
2015-04-07 00:17:04 -0700

    Added async loading.

--- app/index.js
+++ app/index.js
@@ -2,3 +2,21 @@ var api = require('./api');
 var app = require('./app');
 
 window.Taplytics = module.exports = app;
+
+if (window._TLQueue && window._TLQueue instanceof Array) {
+    var queue = window._TLQueue;
+
+    if (queue.length > 0) {
+        for(var i = 0; i < queue.length; i++) {
+            var func = queue[i];
+
+            var func_name = func.shift();
+            var func_args = func;
+
+            if (app[func_name] instanceof Function)
+                app[func_name].apply(app, func_args);
+
+        }
+    }
+
+}
